7cc9253e2acbbdd15055de1c56702216d843ae50
Peter Collingridge <peter.collingridge@nanoporetech.com>
2025-05-07 22:01:29 +0100

    Function to calculation transition probabilities

---
+++ src/build_net.py
@@ -0,0 +1,55 @@
+""" Start testing with a simple example of two, very similar sentences. """
+
+import os
+import torch
+
+from utils.neural_net import train_simple_model, run_model
+from utils.text import (
+    get_hot_one_encoding,
+    get_word_pair_vectors,
+    get_vocab_list,
+    tokenise_sentence,
+)
+from vis.svg_model import NeuralNetSVG
+from data.example1a import sentences
+
+def build_model(sentences, loop_n=100000):
+    """
+    Build a simple neural network model for the given sentences.
+    """
+    # Tokenize the sentences
+    sentence_tokens = [tokenise_sentence(sentence) for sentence in sentences]
+
+    # Get the vocabulary list
+    vocab = get_vocab_list(sentence_tokens)
+
+    # Get the one-hot encoding for each word in the vocabulary
+    encode_word = get_hot_one_encoding(vocab)
+
+    # Get input and output vectors for the word pairs
+    input_vectors, output_vectors = get_word_pair_vectors(sentence_tokens, encode_word)
+
+    # Train the model
+    model = train_simple_model(input_vectors, output_vectors, hidden_size=2, loop_n=loop_n)
+
+    return model, vocab
+
+model, vocab = build_model(sentences, loop_n=30000)
+
+# for layer in model:
+#     print(layer.weight.data)
+
+test_inputs = torch.eye(len(vocab))
+test_outputs = run_model(model, test_inputs)
+
+for i, input_word in enumerate(vocab):
+    output_distribution = {
+        vocab[j]: probability for j, probability in enumerate(test_outputs[i])
+        if probability > 0
+    }
+    # Print the word and its corresponding output
+    print(f"{input_word}: {output_distribution}")
+
+# filename = os.path.join('src', 'vis','example_1b.svg')
+# model_svg = NeuralNetSVG(model, labels=vocab)
+# model_svg.write_to_file(filename, test_inputs)
--- src/utils/neural_net.py
+++ src/utils/neural_net.py
@@ -41,3 +41,19 @@ def train_simple_model(input_vectors, output_vectors, hidden_size = 2, loop_n=10
         optimizer.step()
 
     return model
+
+
+def run_model(model, input_vectors, threshold=0.05):
+    """ Run the model on the input vectors. """
+
+    X = torch.FloatTensor(input_vectors)
+    y_pred = model(X)
+
+    probabilties = torch.softmax(y_pred, dim=-1)
+
+    # Zero out probabilities below the threshold
+    filtered = torch.where(probabilties >= threshold, probabilties, torch.tensor(0.0, device=y_pred.device))
+
+    # Renormalize to make sure the sum is still 1
+    renormormalised = filtered / filtered.sum(dim=-1, keepdim=True)
+    return renormormalised.detach().numpy()
